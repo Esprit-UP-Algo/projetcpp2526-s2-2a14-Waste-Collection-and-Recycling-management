8bae1c31975f19451a48093c84f3b3603d91b25c
amineboukraim <amineboukraim247@gmail.com>
2026-02-09 20:30:11 +0100

    Configuration: Lancement direct de Recyclage et nettoyage CMake

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <QApplication>
-#include "mainwindow.h"
+#include "recyclage.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-
-    MainWindow window;
-    window.resize(1400, 900);
-    window.show();
-
-    return app.exec();
+    QApplication a(argc, argv);
+    Recyclage w;
+    w.showMaximized();
+    return a.exec();
 }
